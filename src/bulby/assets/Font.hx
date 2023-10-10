581e6de1d7ed2889ba6891aeb8d679b75daa8af9
BulbyVR <26726264+TheDrawingCoder-Gamer@users.noreply.github.com>
2023-10-10 14:53:09 -0400

    Wowie zoiw, fonts

--- src/bulby/assets/Font.hx
+++ src/bulby/assets/Font.hx
@@ -1,6 +1,7 @@
 package bulby.assets;
 
 import bulby.assets.Image;
+import sys.io.File;
 /**
   Taken from heaps
   **/
@@ -284,6 +285,64 @@ class Font {
 		if( a == null )
 			return lineHeight - 2 - padding;
 		return a.t.dy + a.t.height - padding;
+	}
+
+}
+
+import haxe.io.Input;
+
+class Reader {
+
+	var i : Input;
+
+	public function new( i : Input ) {
+		this.i = i;
+	}
+
+	public function read( resolveTile: String -> Tile ) : Font {
+
+		if (i.readString(4) != "BFNT" || i.readByte() != 0) throw "Not a BFNT file!";
+
+		var font : Font = null;
+
+		switch (i.readByte()) {
+			case 1:
+				font = new h2d.Font(i.readString(i.readUInt16()), i.readInt16());
+				font.tilePath = i.readString(i.readUInt16());
+				var tile = font.tile = resolveTile(font.tilePath);
+				font.lineHeight = i.readInt16();
+				font.baseLine = i.readInt16();
+				var defaultChar = i.readInt32();
+				var id : Int;
+				while ( ( id = i.readInt32() ) != 0 ) {
+					var t = tile.sub(i.readUInt16(), i.readUInt16(), i.readUInt16(), i.readUInt16(), i.readInt16(), i.readInt16());
+					var glyph = new h2d.Font.FontChar(t, i.readInt16());
+					font.glyphs.set(id, glyph);
+					if (id == defaultChar) font.defaultChar = glyph;
+
+					var prevChar : Int;
+					while ( ( prevChar = i.readInt32() ) != 0 ) {
+						glyph.addKerning(prevChar, i.readInt16());
+					}
+				}
+			case ver:
+				throw "Unknown BFNT version: " + ver;
+		}
+
+		return font;
+	}
+
+	public static inline function parse(bytes : haxe.io.Bytes, resolveTile : String -> Tile ) : Font {
+		return new Reader(new haxe.io.BytesInput(bytes)).read(resolveTile);
+	}
+	public static function parseFont(font: String): Font {
+		final fntFile = './res/Fonts/$font.fnt';
+		if (File.exists(fntFile)) {
+			final bytes = File.getBytes(fntFile);
+			parse(bytes, name => {
+				return Tile.fromImage(Image.fromPng("./res/Fonts/" + name));
+			});
+		}
 	}
 
 }
