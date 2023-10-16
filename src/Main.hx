45f4e0e8e9687ffd13508e84fe469aeedb7b7fd5
BulbyVR <26726264+TheDrawingCoder-Gamer@users.noreply.github.com>
2023-10-16 12:54:27 -0400

    Rework arguments

--- src/Main.hx
+++ src/Main.hx
@@ -2,13 +2,8 @@ package;
 
 import bulby.assets.Text;
 import bulby.assets.FontParser;
-import bulby.assets.Font;
-import bulby.assets.mat.Color;
 import haxe.io.Bytes;
-import bulby.assets.gltf.schema.TGLTF;
-import bulby.assets.Image;
 import haxe.Json;
-import sys.FileSystem;
 import thinghandler.ThingHandler;
 import sys.io.File;
 import Sys.println as println;
@@ -19,40 +14,58 @@ var debug = false;
 
 function main() {
 	var args = Sys.args();
-	#if debugbubly
-	if (args.length == 0)
-		args = ["input.json", "output", "-gltf", "-glb", "-debug"];
-	#end
 	var input = "";
-	var output = "";
+	var outGlb = null;
+	var outGltf = null;
 	var doGltf = false;
 	var doGlb = false;
+	var exportedAreaFolder = null;
+	var cdnAreaFolder = null;
+	var thingFolder = null;
+	var mainID: Null<String> = null;
+	var i = 0;
 	if (args.length < 2) {
 		println("Not enough args.");
 		println("Command usage:");
-		println("al2gltf [input] [output]");
+		println("al2gltf -in [input] -glb [output]");
 		println("Where input is input file");
-		println("and output is output file (no extension)");
+		println("and output is output file");
 		println("Available flags:");
-		println("-gltf: Generate gltf file");
-		println("-glb: Generate glb file");
+		println("-gltf [file]: Generate gltf file");
+		println("-glb [file]: Generate glb file");
+		println("-areaExported [folder]: Import an area exported from anyland");
+		println("-areaCDN [folder]: Import an area dumped from the CDN");
+		println("-inFolder [folder]: Import a thing that contains subthings");
+		println("-id [name]: Required with inFolder. The main thing to be rendered.");
 		println("-debug: Output extra files/print more to commandline");
 		Sys.exit(1);
-	} else {
-		input = args.shift();
-		output = args.shift();
-		for (flag in args) {
-			switch (flag) {
-				case "-gltf":
-					doGltf = true;
-				case "-glb":
-					doGlb = true;
-				case "-debug":
-					debug = true;
-				default:
-					println("Unexpected arg: " + flag);
-					Sys.exit(1);
-			}
+	} 
+
+	while (i < args.length) {
+		final c = i++;
+		switch (args[c]) {
+			case "-in":
+				input = args[i++];
+			case "-glb":
+				outGlb = args[i++];
+				doGlb = true;
+			case "-gltf":
+				outGltf = args[i++];
+				doGltf = true;
+			case "-debug":
+				debug = true;
+			case "-areaExported":
+				exportedAreaFolder = args[i++];
+				throw "Exported areas aren't implemented yet";
+			case "-areaCDN":
+				cdnAreaFolder = args[i++];
+				throw "CDN Areas aren't implemented yet";
+			case "-inFolder":
+				thingFolder = args[i++];
+				throw "Things with subthings aren't implemented yet";
+			case "-id":
+				mainID = args[i++];
+				throw "Things with subthings aren't implemented yet";
 		}
 	}
 	println("Importing Thing...");
@@ -82,9 +95,9 @@ function main() {
 
 	println("Saving files...");
 	if (doGlb)
-		File.saveBytes(output + ".glb", glb);
+		File.saveBytes(outGlb, glb);
 	if (doGltf)
-		File.saveContent(output + ".gltf", Json.stringify(gltf));
+		File.saveContent(outGltf, Json.stringify(gltf));
 	println("Done!");
 	/*
 	if (debug) {
